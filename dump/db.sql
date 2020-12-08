e8b1e993f0a58d8b9b2afdfcc8e2375746f1b24e
Th3-Cod3 <th3-cod3-1230@hotmail.com>
2020-12-08 11:40:25 -0400

    feat(database): create db structure

---
+++ dump/db.sql
@@ -0,0 +1,108 @@
+CREATE DATABASE IF NOT EXISTS dtt;
+USE dtt;
+
+CREATE TABLE IF NOT EXISTS persons (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100) NULL,
+    birthdate DATETIME NOT NULL,
+    gender ENUM('F', 'M', 'U') NOT NULL DEFAULT 'U', -- U = undefined
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS permissions (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_permission_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(15) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    person_id INT NOT NULL UNIQUE,
+    role_id INT NOT NULL,
+    last_login TIMESTAMP NULL,
+    first_fail TIMESTAMP NULL,
+    fail_counter TINYINT(1) NULL,
+    banned ENUM('Y', 'N') NULL DEFAULT 'N',
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id, person_id),
+    CONSTRAINT fk_user_person
+        FOREIGN KEY (person_id)
+        REFERENCES persons(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT fk_user_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS room_types (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS houses (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    street VARCHAR(100) NOT NULL,
+    zip_code VARCHAR(10) NOT NULL,
+    number INT NOT NULL,
+    addition VARCHAR(20) NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_house_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS rooms (
+    id INT NOT NULL AUTO_INCREMENT,
+    house_id INT NOT NULL,
+    type_id INT NOT NULL,
+    width INT NOT NULL,
+    length INT NOT NULL,
+    height INT NOT NULL,
+    deleted_at TIMESTAMP NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_room_house
+        FOREIGN KEY (house_id)
+        REFERENCES houses(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT fk_room_type
+        FOREIGN KEY (type_id)
+        REFERENCES room_types(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE = InnoDB;
