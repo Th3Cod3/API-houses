9920529a3ff936270a28f7b4e73687c18bea34fa
Th3-Cod3 <th3-cod3-1230@hotmail.com>
2020-12-10 00:53:26 -0400

    feat: generate migration

---
+++ src/app/migrations/1.0.0/permissions.php
@@ -0,0 +1,98 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Migrations\Mvc\Model\Migration;
+
+/**
+ * Class PermissionsMigration_100
+ */
+class PermissionsMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('permissions', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'size' => 11,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 100,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'role_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'size' => 11,
+                            'after' => 'name'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('PRIMARY', ['id'], 'PRIMARY'),
+                    new Index('fk_permission_role', ['role_id'], '')
+                ],
+                'references' => [
+                    new Reference(
+                        'fk_permission_role',
+                        [
+                            'referencedTable' => 'roles',
+                            'referencedSchema' => 'dtt',
+                            'columns' => ['role_id'],
+                            'referencedColumns' => ['id'],
+                            'onUpdate' => 'CASCADE',
+                            'onDelete' => 'CASCADE'
+                        ]
+                    )
+                ],
+                'options' => [
+                    'table_type' => 'BASE TABLE',
+                    'auto_increment' => '1',
+                    'engine' => 'InnoDB',
+                    'table_collation' => 'utf8mb4_general_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
